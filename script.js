2da6887810e5d8e5a0b3adb7171e792ea44dae98
diogo <diogohenriquepereirasabino7@gmail.com>
2026-04-29 11:59:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,97 @@
+let carrinho = [];
+let formaPagamento = "";
+
+function addCarrinho(nome, preco){
+carrinho.push({nome, preco});
+renderCarrinho();
+}
+
+function renderCarrinho(){
+
+const lista=document.getElementById("listaCarrinho");
+const totalEl=document.getElementById("total");
+const qtd=document.getElementById("qtdSacola");
+
+lista.innerHTML="";
+let total=0;
+
+carrinho.forEach((item,index)=>{
+
+const li=document.createElement("li");
+li.className="item-carrinho";
+
+li.innerHTML=`
+<span>${item.nome} - R$ ${item.preco.toFixed(2)}</span>
+<button class="btn-excluir" onclick="removerItem(${index})">
+🗑
+</button>
+
+`;
+
+lista.appendChild(li);
+
+total+=item.preco;
+});
+
+totalEl.textContent=`Total: R$ ${total.toFixed(2)}`;
+qtd.textContent=carrinho.length;
+}
+
+function removerItem(index){
+carrinho.splice(index,1);
+renderCarrinho();
+}
+
+function abrirSacola(){
+document.getElementById("modalSacola").style.display="block";
+}
+
+function fecharSacola(){
+document.getElementById("modalSacola").style.display="none";
+}
+
+function abrirPagamento(){
+
+if(carrinho.length===0){
+alert("Adicione itens primeiro!");
+return;
+}
+
+document.getElementById("pagamentoArea").style.display="block";
+}
+
+function selecionarPagamento(tipo){
+
+formaPagamento = tipo;
+
+const pixArea = document.getElementById("pixArea");
+
+if(tipo === "PIX"){
+    pixArea.style.display = "flex";
+}else{
+    pixArea.style.display = "none";
+    enviarWhatsapp();
+}
+}
+
+
+function enviarWhatsapp(){
+
+const numero="5534997909676";
+
+let mensagem="🧁 *Novo Pedido*\n\n";
+let total=0;
+
+carrinho.forEach(item=>{
+mensagem+=`• ${item.nome} - R$ ${item.preco.toFixed(2)}\n`;
+total+=item.preco;
+});
+
+mensagem+=`\n💰 Pagamento: ${formaPagamento}`;
+mensagem+=`\n🧾 Total: R$ ${total.toFixed(2)}`;
+
+window.open(
+`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`,
+"_blank"
+);
+}
